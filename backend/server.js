41ad61a1de58bd23f4f0320369fa0f30a8ae5acf
KKashi-Hatake <shashankgamerid@gmail.com>
2024-03-30 16:34:10 +0530

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
-
-dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // to read coookies on the server side
